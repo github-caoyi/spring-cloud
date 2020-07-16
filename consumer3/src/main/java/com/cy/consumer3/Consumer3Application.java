5d3d377451ae1c693da2491759593068d3e89752
caoyi <3108815218@qq.com>
2020-07-16 10:28:09 +0800

    新加zuul

--- consumer3/src/main/java/com/cy/consumer3/Consumer3Application.java
+++ consumer3/src/main/java/com/cy/consumer3/Consumer3Application.java
@@ -1,12 +1,15 @@
 package com.cy.consumer3;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
@@ -29,12 +32,27 @@ public class Consumer3Application {
         String path = env.getProperty("server.servlet.context-path");
         System.out.println("\n----------------------------------------------------------\n\t" +
                 "Application  is running! Access URLs:\n\t" +
-                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
-                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
-                "swagger-ui: \t\thttp://" + ip + ":" + port + path + "/doc.html\n" +
-                "HystrixDashboard: \t\thttp://" + ip + ":" + port + path + "/hystrix\n" +
+                "Local: \t\thttp://localhost:" + port + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + "/\n\t" +
+                "swagger-ui: \t\thttp://" + ip + ":" + port + "/doc.html\n" +
+                "HystrixDashboard: \t\thttp://" + ip + ":" + port + "/hystrix\n" +
                 "----------------------------------------------------------");
     }
-
+    /**
+     * @Author cy
+     * @Date 2020/7/15 16:26
+     * @Description hystrix仪表盘
+     * @Param
+     * @return
+     **/
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
 }
